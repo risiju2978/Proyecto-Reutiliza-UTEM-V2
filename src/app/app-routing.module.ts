35f65122f454292545afb42205109d87f1d8d5de
risiju <ricardorodriguez2978@gmail.com>
2023-06-22 00:14:36 -0400

    ESTE ES MI PRIMER COMMIT

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { VistaInicioComponent } from './Pagina-de-inicio/vista-inicio/vista-inicio.component';
+import { FooterComponent } from './Pagina-de-inicio/footer/footer.component';
+const routes: Routes = [
+  {path: 'vistaInicio' , component:VistaInicioComponent},
+  {path: 'Footer' , component:FooterComponent},
+];
 
-const routes: Routes = [];
+
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
